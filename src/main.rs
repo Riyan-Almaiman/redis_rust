d1645d3cf89d226396e9c23726297b9f1fbb70fc
Riyan <ralmaiman@gmailcom>
2026-02-19 00:47:43 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![allow(unused_imports)]
 
+use std::io::Read;
+use bytes::Buf;
+use tokio::fs::read;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 
@@ -39,12 +42,19 @@ async fn main() {
             match n {
                 Ok(n) => {
                     buf.append(&mut read_buf[0..n].to_vec());
-                    println!("{}", String::from_utf8_lossy(&buf));
                     if n == 0 {
-                        break; // connection closed
+                        break;
                     }
-                    if bytes_to_string(&buf).contains("PING") {
+                    let current_str = bytes_to_string(&read_buf[0..n]);
+                    if current_str.starts_with("ECHO "){
+                         echo(&mut stream, &buf[0..n]).await;
+                        buf.clear();
+
+                    }
+
+                    if bytes_to_string(&buf) ==  "PING"  {
                         ping(&mut stream).await;
+                        buf.clear();
                     }
                 }
                 Err(e) => {
@@ -54,9 +64,60 @@ async fn main() {
         }
 
     }
+    async fn echo(stream: &mut TcpStream, current_buf: &[u8]) {
+        println!("ECHO");
+        match current_buf.last(){
+            Some(&b) => {
+                if b == b'\n' {
+                    write_echo(stream, &current_buf[5..]).await;
+                }
+            }
+            None => {}
+        }
+        let mut buf = Vec::new();
 
+        loop {
+                let mut read_buf = vec![0; 1024];
+
+                let n = stream.read(&mut read_buf).await;
+                match n {
+                    Ok(n) => {
+                        buf.append(&mut read_buf[0..n].to_vec());
+                        if n == 0 {
+                            break;
+                        }
+                        if buf.len() > 0 {
+                        if(buf[buf.len() - 1] == b'\n') {
+                            write_echo( stream, &buf).await;
+                            break;
+                        }}
+
+                    }
+                    Err(e) => {
+                        break;
+                    }
+                }
+            }
+
+    }
+
+    async fn write_echo(stream: &mut TcpStream, message: &[u8] ){
+
+        let mut response = Vec::new();
+        let clrf = b"\r\n";
+
+        let mut message_length = message.len().to_string().as_str().as_bytes().to_vec();
+        response.push(b'$');
+        response.append(&mut message_length);
+        response.append(clrf.to_vec().as_mut());
+        response.append(message.to_vec().as_mut());
+        response.append(clrf.to_vec().as_mut());
+        if stream.write_all(&response).await.is_err() {
+            println!("Failed to ECHO");
+        };;
+    }
     fn bytes_to_string(bytes: &[u8]) -> String {
-        String::from_utf8_lossy(bytes).to_string()
+        String::from_utf8_lossy(&bytes).to_string()
     }
     async fn ping(stream: &mut TcpStream) {
         if stream.write_all(b"+PONG\r\n").await.is_err() {
