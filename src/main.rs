c519ee9028c12cba0a345c4f0dd4fe708d24dd18
Riyan <ralmaiman@gmailcom>
2026-02-18 22:29:16 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,57 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+
+use std::io::{BufReader, Read, Write};
+use std::net::{TcpListener, TcpStream};
+use tokio::io::AsyncReadExt;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = match TcpListener::bind("127.0.0.1:6379") {
+        Ok(listener) => {
+            listener
+        }
+        Err(e) => {
+            eprintln!("Failed to bind: {}", e);
+            return;
+        }
+    };
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+    for mut stream in listener.incoming() {
+        match &mut  stream {
+            Ok( _stream) => {
+                let mut buf = vec![0; 1024];
+                loop {
+                    let n = _stream.read(&mut buf);
+                    match n {
+                        Ok(n) => {
+                            if n == 0 {
+                                break; // connection closed
+                            }
+                            if bytes_to_string(&buf[0..n]).contains("ping") {
+                                ping(_stream);
+                            }
+                        },
+                        Err(e) => {
+                            break;
+                        }
+                    }
+
+                }
+
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+
+    fn bytes_to_string(bytes: &[u8]) -> String {
+        String::from_utf8_lossy(bytes).to_string()
+    }
+    fn ping(stream: &mut TcpStream ){
+        stream.write_all(b"pong").unwrap();
     }
 }
