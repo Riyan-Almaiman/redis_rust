07f0f2cadf8cf1089b95b618ecbb8d852c5c7a6a
Riyan <ralmaiman@gmailcom>
2026-02-19 02:34:44 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(unused_imports)]
 
-use std::io::Read;
+use std::io::{stdout, Read};
+use std::path::Path;
 use bytes::Buf;
 use tokio::fs::read;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
@@ -37,25 +38,21 @@ async fn main() {
         let mut buf = Vec::new();
         loop {
             let mut read_buf = vec![0; 1024];
-
+            let current = bytes_to_string(&buf);
             let n = stream.read(&mut read_buf).await;
             match n {
                 Ok(n) => {
                     buf.append(&mut read_buf[0..n].to_vec());
+                    let succeeded = parse_array(&buf, &mut stream).await;
+                    if succeeded {
+                        buf.clear();
+                        println!("Successfully read {} bytes", buf.len());
+                        print!("{}[2J{}[1;1H", 27 as char, 27 as char);
+                    }
                     if n == 0 {
                         break;
                     }
-                    let current_str = bytes_to_string(&read_buf[0..n]);
-                    if current_str.starts_with("ECHO "){
-                         echo(&mut stream, &buf[0..n]).await;
-                        buf.clear();
 
-                    }
-
-                    if bytes_to_string(&buf) ==  "PING"  {
-                        ping(&mut stream).await;
-                        buf.clear();
-                    }
                 }
                 Err(e) => {
                     break;
@@ -64,45 +61,11 @@ async fn main() {
         }
 
     }
-    async fn echo(stream: &mut TcpStream, current_buf: &[u8]) {
-        println!("ECHO");
-        match current_buf.last(){
-            Some(&b) => {
-                if b == b'\n' {
-                    write_echo(stream, &current_buf[5..]).await;
-                }
-            }
-            None => {}
-        }
-        let mut buf = Vec::new();
 
-        loop {
-                let mut read_buf = vec![0; 1024];
-
-                let n = stream.read(&mut read_buf).await;
-                match n {
-                    Ok(n) => {
-                        buf.append(&mut read_buf[0..n].to_vec());
-                        if n == 0 {
-                            break;
-                        }
-                        if buf.len() > 0 {
-                        if(buf[buf.len() - 1] == b'\n') {
-                            write_echo( stream, &buf).await;
-                            break;
-                        }}
-
-                    }
-                    Err(e) => {
-                        break;
-                    }
-                }
-            }
-
-    }
 
     async fn write_echo(stream: &mut TcpStream, message: &[u8] ){
 
+        println!("Sending {} bytes", bytes_to_string(&message));
         let mut response = Vec::new();
         let clrf = b"\r\n";
 
@@ -110,8 +73,8 @@ async fn main() {
         response.push(b'$');
         response.append(&mut message_length);
         response.append(clrf.to_vec().as_mut());
-        response.append(message.to_vec().as_mut());
         response.append(clrf.to_vec().as_mut());
+        response.append(message.to_vec().as_mut());
         if stream.write_all(&response).await.is_err() {
             println!("Failed to ECHO");
         };;
@@ -123,5 +86,70 @@ async fn main() {
         if stream.write_all(b"+PONG\r\n").await.is_err() {
             println!("Failed to send PONG");
         };
+    }
+
+    async fn parse_array(data: &[u8], stream: &mut TcpStream ) -> bool {
+
+        let mut result: Vec<Vec<u8>> = Vec::new();
+        let mut current = Vec::new();
+        let mut iter = data.iter().enumerate();
+        while let Some((i, &val)) = iter.next() {
+              if val == b'\r' || val == b'\n' {
+                  result.push(current.clone());
+                  iter.next();
+                  current.clear();
+              }else {
+                  current.push(val);
+              }
+
+        }
+        for r in &result {
+            println!("{:?}", bytes_to_string(&r));
+        }
+        if result.len() > 0{
+            match get_number_of_args(&result[0]){
+                Some(n) => {
+                    if (result.len()-1) / 2 == n as usize {
+                         if result.len() > 2{
+                             match bytes_to_string(&result[2]).to_ascii_lowercase().as_str() {
+                                 "ping" =>  {
+                                     ping(stream).await;
+                                     return true;
+                                 },
+                                 "echo"=> {
+                                     match result.get(4) {
+                                         Some(v) => write_echo(stream, v).await,
+                                         None => return true,
+                                     }
+                                 }
+                                 _ => return true,
+                             }
+                         }
+                    }
+                    else{
+                        if (result.len()-1) / 2 < n as usize {
+                            return false;
+                        }
+                    }
+                }
+                None => { return true; }
+            }
+        }
+    false
+
+    }
+
+    fn get_number_of_args(number_of_args: &Vec<u8>) -> Option<u32> {
+        if number_of_args.len() !=2 {
+            return None;
+        };
+        if number_of_args[0] != b'*'{
+            return None;
+        };
+        match (number_of_args[1] as char).to_digit(10){
+            Some(n) => Some(n),
+            None => None
+        }
+
     }
 }
